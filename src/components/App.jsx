c399921f5b2a51491089de78c3e49a991dd101f5
leokyeung <leoyeung99@gmail.com>
2019-11-05 14:20:55 -0800

    Passes minimum Mocha tests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,39 @@
-import VideoList from './VideoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
 
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {videos: exampleVideoData, toPlay: exampleVideoData[0]};
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(video) {
+    console.log('I was clicked');
+    console.log(video);
+    this.setState({toPlay: video});
+  }
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><VideoPlayer video={this.state.toPlay} /></h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><VideoList videos={this.state.videos} callback={this.handleClick}/></h5></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><VideoList videos={exampleVideoData}/></h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 
 
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -5,7 +5,9 @@ var VideoListEntry = (props) => (
     </div>
     <div className="media-body">
 
-      <div className="video-list-entry-title">{props.video.snippet.title}</div>
+      <div className="video-list-entry-title" onClick={() => (props.callback(props.video))}>{props.video.snippet.title}
+      </div>
+
       <div className="video-list-entry-detail">{props.video.snippet.description}</div>
 
     </div>
