ef8ac0c392198733dffd2dd689d30407945627a8
leokyeung <leoyeung99@gmail.com>
2019-11-04 19:03:19 -0800

    Update

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,16 @@
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoListEntry from './VideoListEntry.js';
+
+
 var VideoList = () => (
   <div className="video-list">
+    <div><h5>
+      {exampleVideoData.map(data => <VideoListEntry data={data}/>)}</h5></div>
+
+    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
   </div>
 );
 
